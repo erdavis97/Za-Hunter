b0284fffa5fba6301b9ca187e11e90186cddd0bd
Ethan Davis <edavis@bsd220.org>
2024-02-26 09:26:10 -0600

    Initial Commit

---
+++ Za Hunter/Za_HunterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Za_HunterApp.swift
+//  Za Hunter
+//
+//  Created by Ethan Davis on 2/26/24.
+//
+
+import SwiftUI
+
+@main
+struct Za_HunterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
